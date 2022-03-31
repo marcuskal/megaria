f1af1a22712be99f378f666dd1b1a48c9da11848
beabkal <beabeshete@gmail.com>
2022-03-31 12:36:50 +0300

    added apps to settings

--- megaria/settings.py
+++ megaria/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'volunteers.apps.VolunteersConfig',
+    'charity_orgs.apps.CharityOrgsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
