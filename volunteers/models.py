a0b7130863c6f6829ffc55d6195ab47691005c75
beabkal <beabeshete@gmail.com>
2022-04-07 10:28:26 +0300

    added UserProfile model

--- volunteers/models.py
+++ volunteers/models.py
@@ -15,3 +15,12 @@ class Comment(models.Model):
     created_on = models.DateField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, primary_key=True, verbose_name="user", related_name='profile', on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, blank=True, null=True)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    birth_date = models.DateField(null=True, blank=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/default.png')
