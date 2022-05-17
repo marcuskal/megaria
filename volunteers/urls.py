13dfb027c9382c0b58c3683fda468c9f999fcd7c
beabkal <beabeshete@gmail.com>
2022-05-17 15:50:25 +0300

    added notification functionality

--- volunteers/urls.py
+++ volunteers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, AddDislike, UserSearch, ListFollowers)
+    PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, AddDislike, UserSearch, ListFollowers, AddCommentLike, AddCommentDislike, CommentReplyView, PostNotification, FollowNotification, RemoveNotification)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
@@ -9,9 +9,9 @@ urlpatterns = [
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:post_pk>/comment/delete/<int:pk>/',
          CommentDeleteView.as_view(), name='comment-delete'),
-#     path('post/<int:post_pk>/comment/<int:pk>/like', AddCommentLike.as_view(), name='comment-like'),
-#     path('post/<int:post_pk>/comment/<int:pk>/dislike', AddCommentDislike.as_view(), name='comment-dislike'),
-#     path('post/<int:post_pk>/comment/<int:pk>/reply', CommentReplyView.as_view(), name='comment-reply'),
+    path('post/<int:post_pk>/comment/<int:pk>/like', AddCommentLike.as_view(), name='comment-like'),
+    path('post/<int:post_pk>/comment/<int:pk>/dislike', AddCommentDislike.as_view(), name='comment-dislike'),
+    path('post/<int:post_pk>/comment/<int:pk>/reply', CommentReplyView.as_view(), name='comment-reply'),
     path('post/<int:pk>/like',AddLike.as_view(), name='like' ),
     path('post/<int:pk>/dislike',AddDislike.as_view(), name='dislike' ),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
@@ -21,7 +21,11 @@ urlpatterns = [
          AddFollower.as_view(), name='add-follower'),
     path('profile/<int:pk>/followers/remove',
          RemoveFollower.as_view(), name='remove-follower'),
-    path('search/', UserSearch.as_view(), name='profile-search')
+    path('search/', UserSearch.as_view(), name='profile-search'),
+    path('notification/<int:notification_pk>/post/<int:post_pk>', PostNotification.as_view(), name='post-notification'),
+    path('notification/<int:notification_pk>/profile/<int:profile_pk>', FollowNotification.as_view(), name='follow-notification'),
+    path('notification/delete/<int:notification_pk>', RemoveNotification.as_view(), name='notification-delete'),
+
 
 
 
